878d64d47d231fee7e93c41ee138b0f929cb1220
Sunny Sahu <sunnysahu8210@gmail.com>
2025-03-05 19:16:08 +0530

    JWT Utility Written

--- utils/JWT.js
+++ utils/JWT.js
@@ -1,6 +1,27 @@
-// Sign and Verify
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
 
-//sign
+config();
 
-const SECRET_KEY = process.env.JWT_SECRET;
-console.log(SECRET_KEY);
+//sign the Token
+
+const SERECT_KEY = process.env.JWT_SECRET;
+const signToken = (data) => {
+  try {
+    return jwt.sign(data, SERECT_KEY, { expiresIn: "1h" });
+  } catch (error) {
+    console.error("Error signing token :: ", error);
+    return null;
+  }
+};
+
+//verify the Token
+
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, SERECT_KEY);
+  } catch (error) {
+    console.error("Error verifying token :: ", error);
+    return null;
+  }
+};
