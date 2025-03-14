f863ddb3927be8b147752486f3f62ab3f1462401
Sunny Sahu <sunnysahu8210@gmail.com>
2025-03-14 20:36:02 +0530

    Created shares & like mode || Implemented Post Model Schema

--- models/post.model.js
+++ models/post.model.js
@@ -1,1 +1,45 @@
 //Create post Schema
+
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "User is Required"],
+    trim: true,
+    index: true,
+  },
+  username: {
+    type: Schema.type.ObjectId,
+    ref: "User",
+    required: [true, "Username is Required"],
+    unique: true,
+    trim: true,
+    index: true,
+  },
+  profilePhoto: {
+    type: Schema.type.ObjectId,
+  },
+  postImage: {
+    type: String,
+    required: [true, "Image String is Required"],
+  },
+  postCaption: {
+    type: String,
+  },
+  comments : {
+    type: Schema.type.ObjectId,
+    ref: "Comment",
+  },
+  likes : {
+    type : Schema.type.ObjectId,
+    ref: "Like"
+  },
+  shares : {
+    type : Schema.type.ObjectId,
+    ref: "Share"
+  }
+}, {timestamps: true});
+
+export default mongoose.model.Post || mongoose.model("Post", postSchema);
